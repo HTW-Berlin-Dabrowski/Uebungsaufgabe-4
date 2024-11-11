d04ad1654307533e8d131d940917b885a7535870
talviphoenix <talviphoenix@gmail.com>
2024-11-11 18:30:22 +0100

    add recipe tests

--- src/test/java/RecipeTest.java
+++ src/test/java/RecipeTest.java
@@ -13,17 +13,17 @@ public class RecipeTest {
     private final PrintStream originalOut = System.out;
     Recipe pankace;
     Recipe curry;
-     @BeforeEach
-     void setup() {
-         pankace = new Recipe("Apfelpfannkuchen", 20, 4);
-         curry = new Recipe("Gemüsecurry", 30);
-         System.setOut(new PrintStream(outContent));
-     }
+    @BeforeEach
+    void setup() {
+        pankace = new Recipe("Apfelpfannkuchen", 20, 4);
+        curry = new Recipe("Gemüsecurry", 30);
+        System.setOut(new PrintStream(outContent));
+    }
 
-     @AfterEach
-     void restoreStreams() {
-         System.setOut(originalOut);
-     }
+    @AfterEach
+    void restoreStreams() {
+        System.setOut(originalOut);
+    }
 
     @Test
     void constructorTest() {
@@ -52,20 +52,20 @@ public class RecipeTest {
 
     @Test
     void setRatingTest() {
-         pankace.setRating(5);
-         assertEquals(5, pankace.getRating());
-         curry.setRating(10);
-         assertEquals("Eingabe ungültig"+System.lineSeparator(), outContent.toString());
-         assertEquals(0, curry.getRating());
+        pankace.setRating(5);
+        assertEquals(5, pankace.getRating());
+        curry.setRating(10);
+        assertEquals("Eingabe ungültig"+System.lineSeparator(), outContent.toString());
+        assertEquals(0, curry.getRating());
     }
 
     @Test
     void setCategoryTest() {
-         curry.setCategory("omnivor");
-         assertEquals("omnivor", curry.getCategory());
-         pankace.setCategory("Sweet");
-         assertEquals("Eingabe ungültig"+System.lineSeparator(), outContent.toString());
-         assertNull(pankace.getCategory());
+        curry.setCategory("omnivor");
+        assertEquals("omnivor", curry.getCategory());
+        pankace.setCategory("sweet");
+        assertEquals("Eingabe ungültig"+System.lineSeparator(), outContent.toString());
+        assertNull(pankace.getCategory());
     }
 
     @Test
